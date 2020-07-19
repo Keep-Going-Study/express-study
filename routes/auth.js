6cc29a0bfc15359a8ca3a360dadb4bea21225a35
Keep-Going-HyeonSoo <chs98105@gmail.com>
2020-07-19 01:24:58 +0000

    Multi User - 2-1. 회원 정보 저장

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,12 @@ var sanitizeHtml = require('sanitize-html');
 var express = require('express');
 var router = express.Router();
 
+var low = require('lowdb');
+var FileSync = require('lowdb/adapters/FileSync');
+var adapter = new FileSync('db.json');
+var db = low(adapter);
+
+db.defaults({users:[]}).write();
 
 module.exports = function(passport){
     
@@ -45,17 +51,32 @@ module.exports = function(passport){
         var list = template.list(req.list);
         var html = template.HTML(title,list, 
                     `  
-                        <form action="/auth/login_process" method="post">
-                            <p><input type="text" name="email" placeholder="email"></p>
-                            <p><input type="password" name="pwd" placeholder="password"></p>
-                            <p><input type="password" name="pwd2" placeholder="password check"></p>
-                            <p><input type="text" name="displayName" placeholder="Nickname"></p>
+                        <form action="/auth/register_process" method="post">
+                            <p><input type="text" name="email" placeholder="email" value="chs98105@gmail.com"></p>
+                            <p><input type="password" name="pwd" placeholder="password" value="9815"></p>
+                            <p><input type="password" name="pwd2" placeholder="password check" value="9815"></p>
+                            <p><input type="text" name="displayName" placeholder="Nickname" value="soul"></p>
                             <p><input type="submit" value="register"></p>
                         </form>
                     `,'');
         res.send(html);
     });
      
+    router.post('/register_process', function(req,res){
+       var post = req.body;
+       var email = post.email;
+       var pwd = post.pwd;
+       var pwd2 = post.pwd2;
+       var displayName = post.displayName;
+       
+       db.get('users').push({
+           email:email,
+           password:pwd,
+           displayName:displayName
+       }).write();
+       
+       res.redirect('/');
+    });
 
     router.get('/logout', function(req,res){
         req.logout();
