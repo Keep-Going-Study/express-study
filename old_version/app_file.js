91200f88a8c97df97eaa5f0b98344a3a31d0fbc4
Keep-Going-HyeonSoo <chs98105@gmail.com>
2020-08-01 03:31:14 +0000

    구 버전-파일시스템(라우팅&본문저장)

---
+++ old_version/app_file.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+var bodyParser= require('body-parser');
+var fs = require("fs");
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.set('views','./views_file');
+app.set('view engine', 'pug');
+
+app.get('/topic/new', function(req,res){
+    res.render('new.pug');
+});
+
+app.post('/topic',function(req,res){
+    var title= req.body.title;
+    var description = req.body.description;
+    fs.writeFile(`data/${title}`, description, function(err){
+        if(err){
+            res.status(500).send('Server Error');
+        }
+        else
+            res.send('Success');
+    });
+});
+
+app.listen(8080,function(){
+    console.log('Connected, 8080 port!');
+})
