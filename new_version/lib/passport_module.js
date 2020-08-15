1f9e658285677a75033a2a834ee7622d8f8a2cc1
Keep-Going-HyeonSoo <chs98105@gmail.com>
2020-08-15 08:05:25 +0000

    Google Login - 사용자 추가 1

--- new_version/lib/passport_module.js
+++ new_version/lib/passport_module.js
@@ -76,17 +76,33 @@ module.exports = function(app){
         callbackURL: googleCredentials.web.redirect_uris[0]
       },
       function(accessToken, refreshToken, profile, done) {
-           User.findOrCreate({ googleId: profile.id }, function (err, user) {
-             return done(err, user);
-           });
+        console.log('profile : ',profile);
+        
+        var email = profile.emails[0].value;
+        var user = db.get('users').find({email:email}).value();
+        user.googleProfile = profile._json;
+        db.get('users').find({id:user.id}).assign(user).write(); //user 정보 최신화
+        done(null,user);
+            
       }
     ));
     
     // /auth/google 로 접속하면 passport 가 만들어주는 redirect 경로로 이동함
     app.get('/auth/google',
         passport.authenticate('google', {
-            scope: ['https://www.googleapis.com/auth/plus.login']
+            scope: ['https://www.googleapis.com/auth/plus.login', // google login
+                    'https://www.googleapis.com/auth/userinfo.email' // user email 얻기
+                    ]
         }));
+     
+    // google login 이후 리다이렉션 되는 페이지    
+    app.get('/auth/google/callback',
+        passport.authenticate('google', {
+            failureRedirect: '/auth/login'
+        }),
+        function (req, res) {
+            res.redirect('/');
+        });
     
     return passport;
 };
